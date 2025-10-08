0a0426c31c6c4c388756d882619e6a4d54e98ff6
hafarna03aja-droid <hafarna03aja@gmail.com>
2025-10-08 13:27:27 +0700

    Fix: Penyederhanaan konfigurasi Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./screens/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
